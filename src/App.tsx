b49038a6c3ab2cd2c50815414ce737c39a32ea56
manhquan2893 <manhquan2893@gmail.com>
2021-06-07 20:25:05 +0700

    nt

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,11 @@ import React, { useEffect } from 'react';
 
 function App() {
   useEffect(() => {
+    alert('is it?')
   }, [])
   return (
     <div className="App" >
-      <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              <script async src="https://www.googletagmanager.com/gtag/js?id=G-NNRHWPYJFR"></script>
-              <script>
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-NNRHWPYJFR');
-              </script>
-              `,
-            }}
-          />
+     
     </div>
   );
 }
