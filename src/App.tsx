ee8e85affaf7f3f101b34f3db0b5bc1dde337e7a
manhquan2893 <manhquan2893@gmail.com>
2021-06-07 20:21:51 +0700

    nt

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,22 @@ import React, { useEffect } from 'react';
 
 function App() {
   useEffect(() => {
-    alert('My name is Quân')
   }, [])
   return (
     <div className="App" >
-      
+      <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              <script async src="https://www.googletagmanager.com/gtag/js?id=G-NNRHWPYJFR"></script>
+              <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-NNRHWPYJFR');
+              </script>
+              `,
+            }}
+          />
     </div>
   );
 }
