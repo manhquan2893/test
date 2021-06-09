70fb2faa01067711ac19af3a374b33a5d4497cef
manhquan2893 <manhquan2893@gmail.com>
2021-06-10 06:33:52 +0700

    all

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React, { useEffect } from 'react';
 function App() {
   useEffect(() => {
-    
+    //@ts-ignore
+    const dataLayer = window.dataLayer
+    dataLayer.push({ ecommerce: null });  // Clear the previous ecommerce object.
+dataLayer.push({
+  'ecommerce': {
+    'detail': {
+      'actionField': {'list': 'Apparel Gallery'},    // 'detail' actions have an optional list property.
+      'products': [{
+        'name': 'Triblend Android T-Shirt',         // Name or ID is required.
+        'id': '12345',
+        'price': '15.25',
+        'brand': 'Google',
+        'category': 'Apparel',
+        'variant': 'Gray'
+       }]
+     }
+   }
+});
   }, [])
   return (
     <div>
