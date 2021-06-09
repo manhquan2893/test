b7e3d43d4899441b0de8a3721473b8b8934303bc
vmo-longnh <leo@vmodev.com>
2021-06-09 08:43:50 +0700

    nt

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,21 @@ import React, { useEffect } from 'react';
 
 function App() {
   useEffect(() => {
-    alert('is it?')
+    //@ts-ignore
+    const dataLayer = window.dataLayer
+    dataLayer.push({ ecommerce: null });  // Clear the previous ecommerce object.
+  dataLayer.push({
+    'event': 'productClick',
+    'ecommerce': {
+      'click': {
+        'products': [{
+          'name': 'product 1',                      // Name or ID is required.
+          'id': 1,
+          'price': 20,
+         }]
+       }
+     },
+  });
   }, [])
   return (
     <div className="App" >
