2a4da639a4516f29aeb527fd273357bd66e033ac
manhquan2893 <manhquan2893@gmail.com>
2021-06-09 20:27:03 +0700

    all

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React, { useEffect } from 'react';
 
 function App() {
-  useEffect(() => {
-    alert('is it?')
-  }, [])
   return (
     <div className="App" >
-     waefawefawefawfe
+     hello
     </div>
   );
 }
