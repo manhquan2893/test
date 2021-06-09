3df2180d8469f4bb121ad295e77d38f051cb0cf4
manhquan2893 <manhquan2893@gmail.com>
2021-06-10 05:35:37 +0700

    all

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
+  useEffect(() => {
+    
+  }, [])
   return (
-    <div className="App" >
-     hello
+    <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/users">
+          asasas
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
