12b520a1604525cab6c75b9b1086e2fd727a2701
vmo-longnh <leo@vmodev.com>
2021-06-10 10:03:14 +0700

    ???????

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,19 @@ function App() {
     //@ts-ignore
     const dataLayer = window.dataLayer
     dataLayer.push({ ecommerce: null });  // Clear the previous ecommerce object.
-dataLayer.push({
-  'ecommerce': {
-    'detail': {
-      'products': [{
-        'name': 'Triblend Androdsdid T-Shirt',         // Name or ID is required.
-        'id': '12345',
-        'price': '15.25',
-        'brand': 'Googlafsde',
-        'category': 'Appdasarel',
-       }]
-     }
-   }
-});
+    dataLayer.push({
+      'event': 'EEproductClick',
+      'ecommerce': {
+        'click': {
+          'actionField': {'list': 'Search Results'},      // Optional list property.
+          'products': [{
+            'name': 'Triblend Android T-Shirt',       // Name or ID is required.
+            'id': '12345',
+            'price': '15.25'
+           }]
+         }
+       }
+    });
   }, [])
   return (
     <div>
