dac12aac4a8ac52d64652a739520c12a3f1d4b44
vmo-longnh <leo@vmodev.com>
2021-06-10 13:57:33 +0700

    hghgh

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
 import React, { useEffect } from 'react';
 function App() {
   useEffect(() => {
-    //@ts-ignore
-    const dataLayer = window.dataLayer
-    dataLayer.push({ ecommerce: null });  // Clear the previous ecommerce object.
-    dataLayer.push({
-      'event': 'EEproductClick',
-      'ecommerce': {
-        'click': {
-          'actionField': {'list': 'Search Results'},      // Optional list property.
-          'products': [{
-            'name': 'Triblend Android T-Shirt',       // Name or ID is required.
-            'id': '12345',
-            'price': '15.25'
-           }]
-         }
-       }
-    });
+    
   }, [])
   return (
     <div>
