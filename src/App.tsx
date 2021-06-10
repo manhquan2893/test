8af163a14d9f7a192e3725438491385c0848d75f
vmo-longnh <leo@vmodev.com>
2021-06-10 09:38:09 +0700

    gfgf

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,12 @@ function App() {
 dataLayer.push({
   'ecommerce': {
     'detail': {
-      'actionField': {'list': 'Apparel Gallery'},    // 'detail' actions have an optional list property.
       'products': [{
-        'name': 'Triblend Android T-Shirt',         // Name or ID is required.
+        'name': 'Triblend Androdsdid T-Shirt',         // Name or ID is required.
         'id': '12345',
         'price': '15.25',
-        'brand': 'Google',
-        'category': 'Apparel',
-        'variant': 'Gray'
+        'brand': 'Googlafsde',
+        'category': 'Appdasarel',
        }]
      }
    }
