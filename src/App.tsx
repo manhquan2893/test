ce8a13419d40925f7bdebcd56ba02af03bfdaf19
vmo-longnh <leo@vmodev.com>
2021-06-10 09:23:22 +0700

    grrg

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,22 @@ function App() {
     //@ts-ignore
     const dataLayer = window.dataLayer
     dataLayer.push({ ecommerce: null });  // Clear the previous ecommerce object.
-  dataLayer.push({
-    'event': 'EEproductClick',
-    'ecommerce': {
-      'click': {
-        'actionField': {'list': 'Search Results'},      // Optional list property.
-        'products': [{
-          'name': 'Triblend Android T-Shirt',       // Name or ID is required.
-          'id': '12345',
-          'price': '15.25'
-         }]
-       }
+dataLayer.push({
+  'ecommerce': {
+    'detail': {
+      'actionField': {'list': 'Apparel Gallery'},    // 'detail' actions have an optional list property.
+      'products': [{
+        'name': 'Triblend Android T-Shirt',         // Name or ID is required.
+        'id': '12345',
+        'price': '15.25',
+        'brand': 'Google',
+        'category': 'Apparel',
+        'variant': 'Gray'
+       }]
      }
-  });
+   }
+});
   }, [])
-  
   return (
     <div>
       <a href="/users">151515</a>
